9084eb840ef6aadb9d57c12d9e323cf5bb2ab0b9
Terry <bengbengle@gmail.com>
2023-10-19 12:21:06 +0800

    add jest for test

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  roots: ['<rootDir>/tests'],
+  testMatch: [
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  moduleDirectories: ['node_modules', 'src'],
+  testTimeout: 30000,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/*.{ts,tsx}", // Include TypeScript files only
+    "!**/index.{ts,tsx}", // Exclude index.ts
+    "!**/node_modules/**" // Exclude node_modules
+  ],
+  coverageReporters: ["text", "lcov"],
+}
---
+++ tests/subscription.test.ts
@@ -0,0 +1,21 @@
+import {
+    // createSubscription,
+    // getSubscription,
+    // depositFromSender,
+    // withdrawFromRecipient
+} from '../src'
+
+describe('createSubscription', () => {
+
+
+    // TODO: 
+    // 1. init sdk
+    // 2. call sdk's createSubscription func
+
+    console.log('createSubscription')
+
+    test('createSubscription can works', async () => {
+
+    })
+
+})
