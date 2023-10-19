4176c2290bbf1b1e317d486a2d74b1be26d6fb05
Arthur Emmanuel <emmanuel2002arthur@gmail.com>
2023-10-19 09:19:30 +0000

    Feat: start time and stop time test calculation

--- tests/subscription.test.ts
+++ tests/subscription.test.ts
@@ -16,16 +16,18 @@ describe('createSubscription', () => {
 
     test('createSubscription can work', async () => {
         try {
-            // Call the createSubscription function with valid input
+            const currentTimestamp = Math.floor(Date.now() / 1000); // Current timestamp in seconds
+            const interval = 3600 * 24; // One day in seconds
             const input = {
                 recipient: '0xbDf6Fb9AF46712ebf58B9CB0c23B4a881BF58099',
                 deposit: BigInt(100),
                 tokenAddress: '0x949bEd886c739f1A3273629b3320db0C5024c719',
-                startTime: BigInt(12345),
-                stopTime: BigInt(67890),
-                interval: BigInt(60),
-                fixedRate: BigInt(1),  
+                startTime: BigInt(currentTimestamp + 3600), // One hour from now
+                stopTime: BigInt(currentTimestamp + 3600 * 24 * 7,), // One week from startTime
+                interval: BigInt(interval),
+                fixedRate: BigInt(1),
             };
+            
             await createSubscription(input);
         } catch (error) {
             console.log(error);
