5128a2e9d7961931d2d41378c4d291f5d864a60f
Arthur Emmanuel <emmanuel2002arthur@gmail.com>
2023-10-20 08:56:38 +0000

    Feat: additional checks added for create subscription

--- tests/subscription.test.ts
+++ tests/subscription.test.ts
@@ -1,11 +1,10 @@
-import { ethers } from 'ethers';
+import { ethers} from 'ethers';
 import { createSubscription } from '../src';
 
-
 describe('createSubscription', () => {
-    let provider: any
+    let provider:any;
 
-    beforeAll(async () => {
+    beforeAll(() => {
         const infuraUrl = 'https://sepolia.infura.io/v3/577e58eea0d74c13b627c1e3808cd711';
         provider = new ethers.JsonRpcProvider(infuraUrl);
     });
@@ -15,23 +14,49 @@ describe('createSubscription', () => {
     });
 
     test('createSubscription can work', async () => {
+        // Arrange
+        const currentTimestamp = Math.floor(Date.now() / 1000); // Current timestamp in seconds
+        const interval = 3600 * 24; // One day in seconds
+        const input = {
+            recipient: '0xbDf6Fb9AF46712ebf58B9CB0c23B4a881BF58099',
+            deposit: BigInt(1),
+            tokenAddress: '0x949bEd886c739f1A3273629b3320db0C5024c719',
+            startTime: BigInt(currentTimestamp + 3600), // One hour from now
+            stopTime: BigInt(currentTimestamp + 3600 * 24 * 7), // One week from startTime
+            interval: BigInt(interval),
+            fixedRate: BigInt(1),
+        };
+    
+        // Act and Assert
         try {
-            const currentTimestamp = Math.floor(Date.now() / 1000); // Current timestamp in seconds
-            const interval = 3600 * 24; // One day in seconds
-            const input = {
-                recipient: '0xbDf6Fb9AF46712ebf58B9CB0c23B4a881BF58099',
-                deposit: BigInt(1),
-                tokenAddress: '0x949bEd886c739f1A3273629b3320db0C5024c719',
-                startTime: BigInt(currentTimestamp + 3600), // One hour from now
-                stopTime: BigInt(currentTimestamp + 3600 * 24 * 7,), // One week from startTime
-                interval: BigInt(interval),
-                fixedRate: BigInt(1),
-            };
-            
+            // Validation Checks
+            if (!input.recipient) {
+                throw new Error('Recipient is undefined. Please provide a valid recipient address.');
+            }
+    
+            if (input.stopTime < input.startTime) {
+                throw new Error('Stop time cannot be earlier than start time.');
+            }
+    
+            if (input.deposit <= BigInt(0)) {
+                throw new Error('Deposit amount must be greater than zero.');
+            }
+    
+            const userBalance = await provider.getBalance(input.recipient);
+            const depositAmount = ethers.parseEther(input.deposit.toString());
+    
+            if (userBalance && depositAmount && userBalance.lt(depositAmount + input.fixedRate)) {
+                throw new Error('User balance is insufficient to create the subscription.');
+            }
             await createSubscription(input);
+    
+            // Additional assertions for success if needed
+            expect(true).toBeTruthy();
         } catch (error) {
-            console.log(error);
+            // Assert for failure
+            expect(error).toBeNull(); // Assert that no error was thrown
         }
     });
-
+    
 });
+
