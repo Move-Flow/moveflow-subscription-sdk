ff5f105ef926f048d334bb35a4916f0af69fffdb
Arthur Emmanuel <emmanuel2002arthur@gmail.com>
2023-11-06 08:27:33 +0000

    update

--- src/sdk.ts
+++ src/sdk.ts
@@ -18,7 +18,7 @@ import createGraphQLClient, { ChainType } from "./utils/api/client-config";
 
 // Ethereum provider URL and contract information
 
-const privateKey = "753e10bc305827ad956b98c178ed80b0c98900d40a6ecec3e05fe373ad9f85a3";
+const privateKey = "Your_private_key";
 const chain = Chain.Goerli; // Set the chain here (e.g., Sepolia, Goerli)
 const provider = initializeProvider(chain); // Initialize the provider
 const wallet = new ethers.Wallet(privateKey, provider);
--- tests/subscription.test.ts
+++ tests/subscription.test.ts
@@ -13,8 +13,7 @@ import { initializeProvider } from "../src/utils/chain";
 import coinAddressStore from "../src/utils/contractAddress";
 // import client from "../src/utils/api/client-config";
 
-const privateKey =
-  "753e10bc305827ad956b98c178ed80b0c98900d40a6ecec3e05fe373ad9f85a3";
+const privateKey = "Your_private_key";
 const chain = Chain.Goerli; // Set the chain here (e.g., Sepolia, Goerli)
 const provider = initializeProvider(chain); // Initialize the provider
 const wallet = new ethers.Wallet(privateKey, provider);
